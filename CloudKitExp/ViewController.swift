e2e3a530a5a458edd1cf15a88460dab9c88be63b
hafizhMo <hafizh.hizb@gmail.com>
2022-10-02 15:40:53 +0700

    Initial Commit

---
+++ CloudKitExp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloudKitExp
+//
+//  Created by Hafizh Mo on 02/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
